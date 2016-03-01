d58e656531bc54bc3ba2c0792d4350af3d0aa92f
FranciscoP <franciscopresencia@hotmail.com>
2016-03-01 20:43:50 +0900

    forced

--- modules/test/controller.js
+++ modules/test/controller.js
@@ -24,7 +24,6 @@ exports.index = function(req, res, next) {
 
 // Add a test
 exports.add = function(req, res, next) {
-
   pipe(req.body, { user: req.user, language: req.lang })
     .pipe(api.user.auth, config.auth.add)
     .pipe(model.add)

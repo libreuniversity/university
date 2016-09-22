8b64529452b0a895b93626071f5bea92befa4bc9
Francisco Presencia Fandos <developerfrancisco@hotmail.com>
2016-09-23 08:26:54 +0900

    Recover sweet justice but after math rendering

--- public/javascript.js
+++ public/javascript.js
@@ -1354,9 +1354,6 @@ u("form.login").ajax(function(err, data){
   }
 });
 
-// Justify everything that has the class .sweet-justice
-// justify_my_love(document.querySelector(".sweet-justice"));
-
 // Display the mathematics on the pagee
 try {
   renderMathInElement(document.body, { delimiters: [
@@ -1366,6 +1363,9 @@ try {
 } catch (e) {
   console.log(e);
 }
+
+// Justify everything that has the class .sweet-justice
+justify_my_love(document.querySelector(".sweet-justice"));
 
 // / internal
 // "http://www.libre.university/" internal

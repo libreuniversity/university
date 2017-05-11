2e5e6770764e8e6f3e1fee9f0c4ad7dd11637f77
Francisco Presencia Fandos <developerfrancisco@hotmail.com>
2017-05-11 21:56:48 +0200

    Added support for more languages

--- modules/lesson/sanitize.js
+++ modules/lesson/sanitize.js
@@ -1,12 +1,17 @@
 var sanitize = require('sanitize-html');
+var languages = [
+  'apache', 'bash', 'coffeescript', 'cpp', 'cs', 'css', 'diff', 'html', 'http',
+  'ini', 'java', 'javascript', 'json', 'makefile', 'markdown', 'nginx', 'perl',
+  'objectivec', 'php', 'python', 'ruby', 'sql', 'vbscript', 'xhtml', 'xml'
+];
 
 var sanitizeOpt = {
   allowedTags: ['h2', 'h3', 'h4', 'br', 'p', 'strong', 'em', 'a', 'ul', 'li', 'pre', 'code', 'img', 'blockquote'],
   allowedAttributes: { 'a': ['href'], img: ['src'] },
   allowedClasses: {
     'span': ['math-tex'],
-    'pre': ['js', 'javascript', 'css', 'html', 'pug', 'jade'].map(name => 'language-' + name),
-    'code': ['js', 'javascript', 'css', 'html', 'pug', 'jade'].map(name => 'language-' + name)
+    'pre': languages.map(name => 'language-' + name),
+    'code': languages.map(name => 'language-' + name)
   }
 };
 
